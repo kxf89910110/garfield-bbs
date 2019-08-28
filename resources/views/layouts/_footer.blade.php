909e2f847e9c11f7fc30e7c89fb0c5a6ee54c560
kxf89910110 <1052831447@qq.com>
2019-08-28 09:04:39 +0000

    static page

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      Designed and encoded by <a href="#" target="_blank">Moose</a><span style="color: #e27575;font-size: 14px;">⚜</span>
+    </p>
+
+    <p class="float-right">
+      <a href="#">Contact me</a>
+    </p>
+  </div>
+</footer>
