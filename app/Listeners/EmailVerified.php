1c90b4492489f780f352a2d6a98da15ead24b543
kxf89910110 <1052831447@qq.com>
2019-08-29 01:24:17 +0000

    verify success session

---
+++ app/Listeners/EmailVerified.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class EmailVerified
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Verified  $event
+     * @return void
+     */
+    public function handle(Verified $event)
+    {
+        // Message reminder after successful flash authentication in the session
+        session()->flash('success', 'Email verification succeeded ^_^');
+    }
+}
