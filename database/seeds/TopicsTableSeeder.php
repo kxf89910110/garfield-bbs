1dd0171bf3994f5a8407baa98801dd9bf5362ed6
kxf89910110 <1052831447@qq.com>
2019-08-29 09:40:50 +0000

    Populate user and topic data

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -2,19 +2,37 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
+use App\Models\User;
+use App\Models\Category;
 
 class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+        // An array of all user IDs, such as: [1,2,3,4]
+        $user_ids = User::all()->pluck('id')->toArray();
+
+        //  An array of all classification IDs, such as: [1,2,3,4]
+        $category_ids = Category::all()->pluck('id')->toArray();
+
+        // Get a Faker instance
+        $faker = app(Faker\Generator::class);
+
+        $topics = factory(Topic::class)
+                        ->times(50)
+                        ->make()
+                        ->each(function ($topic, $index)
+                            use ($user_ids, $category_ids, $faker)
+        {
+            // Randomly take one from the user ID array and assign it.
+            $topic->user_id = $faker->randomElement($user_ids);
+
+            // Topic classification, ibid.
+            $topic->category_id = $faker->randomElement($category_ids);
         });
 
+        // Convert the data collection to an array and insert it into the database.
         Topic::insert($topics->toArray());
     }
-
 }
 
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Get a Faker instance
+        $faker = app(Faker\Generator::class);
+
+        // Avatar fake data
+        $avatars = [
+            'https://pic1.zhimg.com/v2-4c7e350e627ac0ddac40e3cd5766d42c_r.jpg',
+
+            'https://pic2.zhimg.com/v2-64f436d9077e502a973279409b1feea5_r.jpg',
+
+            'https://pic2.zhimg.com/v2-5f7c00c4b1687de2e320f5629910402d_r.jpg',
+
+            'https://pic4.zhimg.com/80/v2-65da9bccd3d71baad0c82bc2d73d3e9b_hd.jpg',
+
+            'https://pic4.zhimg.com/80/v2-0d1124be717f26f337ed69985ce16927_hd.jpg',
+
+            'https://pic4.zhimg.com/80/v2-58ce894dbaf02d1572c8c163b6aaa74f_hd.jpg'
+        ];
+
+        // Generating data sets
+        $users = factory(User::class)
+                        ->times(10)
+                        ->make()
+                        ->each(function ($user, $index)
+                            use ($faker, $avatars)
+        {
+            // Randomly take one from the avatar array and assign it
+            $user->avatar = $faker->randomElement($avatars);
+        });
+
+        // Make hidden fields visible and convert data collections to arrays
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        // Insert into the database
+        User::insert($user_array);
+    }
+}
