39dfa73fbbb5fc90b64147679f5e4af3f2a48108
kxf89910110 <1052831447@qq.com>
2019-08-30 01:50:15 +0000

    Topic sorting

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -15,4 +15,33 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function scopeWithOrder($query, $order)
+    {
+        // Different sorts, using different data read logic
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->recentReplied();
+                break;
+        }
+        // Preload prevents N+1 issues
+        return $query->with('user', 'category');
+    }
+
+    public function scopeRecentReplied($query)
+    {
+        // When the topic has a new reply, we will write logic to update the reply_count attribute of the topic model
+        // The framework automatically triggers the update of the data model updated_at timestamp
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeRecent($query)
+    {
+        // Sort by creation time
+        return $query->orderBy('created_at', 'desc');
+    }
 }
