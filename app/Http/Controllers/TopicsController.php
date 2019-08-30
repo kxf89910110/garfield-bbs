0ee84e60035407a3c54e264b478cdf0e8afee88e
kxf89910110 <1052831447@qq.com>
2019-08-30 07:53:45 +0000

    delete topic

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -63,7 +63,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('success', 'Successfully deleted');
 	}
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
