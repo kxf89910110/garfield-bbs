7630dd7f5f867ff6ed03fe062cf0c2890f7bef17
kxf89910110 <1052831447@qq.com>
2019-09-02 06:09:19 +0000

    message notification reply

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,30 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
+use Auth;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use Notifiable, MustVerifyEmailTrait;
+    use MustVerifyEmailTrait;
+
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
+
+    public function notify($instance)
+    {
+        // If the person to be notified is the current user, there is no need to notify.
+        if ($this->id == Auth::id()) {
+            return;
+        }
+
+        // Only need to remind you of the database type notification, send email directly or other Pass
+        if (method_exists($instance, 'toDatabase')) {
+            $this->increment('notification_count');
+        }
+
+        $this->laravelNotify($instance);
+    }
 
     /**
      * The attributes that are mass assignable.
