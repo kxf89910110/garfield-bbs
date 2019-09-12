24017e968c573aee178fe84555e9e80595cd5c31
kxf89910110 <1052831447@qq.com>
2019-09-12 03:04:39 +0000

    backImage

--- resources/views/pages/root.blade.php
+++ resources/views/pages/root.blade.php
@@ -2,5 +2,8 @@
 @section('title', 'Home')
 
 @section('content')
-  <h1>This is home</h1>
+  <div class="text-center">
+    <h1>Welcome to the Garfield BBS</h1>
+    <img width="910px" height="540px" src="https://raw.githubusercontent.com/kxf89910110/My-Blog/master/img/reg.jpg">
+  </div>
 @stop
