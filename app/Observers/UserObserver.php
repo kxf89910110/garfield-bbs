c70897434a90cc98055c974cd2be6d841d961ed9
kxf89910110 <1052831447@qq.com>
2019-09-12 09:11:33 +0000

    default avatar

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -4,18 +4,13 @@ namespace App\Observers;
 
 use App\Models\User;
 
-// creating, created, updating, updated, saving,
-// saved,  deleting, deleted, restoring, restored
-
 class UserObserver
 {
-    public function creating(User $user)
+    public function saving(User $user)
     {
-        //
-    }
-
-    public function updating(User $user)
-    {
-        //
+        // This is more scalable, and the default avatar is only specified when it is empty.
+        if (empty($user->avatar)) {
+            $user->avatar = 'https://raw.githubusercontent.com/kxf89910110/garfield-bbs/master/public/uploads/images/avatars/TrJS40Ey5k.png';
+        }
     }
 }
