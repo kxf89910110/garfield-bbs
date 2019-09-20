9929d6a9761c12f6827537dd80b928d50468cd35
kxf89910110 <1052831447@qq.com>
2019-09-20 07:42:50 +0000

    permissions index

--- routes/api.php
+++ routes/api.php
@@ -102,6 +102,9 @@ $api->version('v1', [
             // Mark message notification as read
             $api->patch('user/read/notifications', 'NotificationsController@read')
                 ->name('api.user.notifications.read');
+            //
+            $api->get('user/permissions', 'PermissionsController@index')
+                ->name('api.user.permissions.index');
         });
     });
 });
